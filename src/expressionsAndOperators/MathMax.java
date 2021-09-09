a0251139291a0b90137f97a07e47a1ad4a748d76
Ryo-exid <sakura4oneok@gmail.com>
2021-09-09 17:06:23 +0900

    大きい方の数値を代入する命令

---
+++ src/expressionsAndOperators/MathMax.java
@@ -0,0 +1,13 @@
+package expressionsAndOperators;
+
+public class MathMax {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		
+		// 2つの値を比較して大きい方の数値を代入する
+		int m = Math.max(a, b);
+		
+		System.out.println(a + "と" + b + "とで大きい方は・・・" + m);
+	}
+}
