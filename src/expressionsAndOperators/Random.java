941840dab38f3646d6809e6e06bb7792763385e5
Ryo-exid <sakura4oneok@gmail.com>
2021-09-09 17:21:10 +0900

    ランダムな数を生成する命令

---
+++ src/expressionsAndOperators/Random.java
@@ -0,0 +1,8 @@
+package expressionsAndOperators;
+
+public class Random {
+	public static void main(String[] args) {
+		int r = new java.util.Random().nextInt(1000);
+		System.out.println("与ダメージ：" + r);
+	}
+}
