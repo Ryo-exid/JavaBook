25b876b504fefeae32f078ae22f1795e2f30720b
Ryo-exid <sakura4oneok@gmail.com>
2021-09-11 19:01:56 +0900

    拡張For文

--- src/array/ExtentionFor.java
+++ src/array/ExtentionFor.java
@@ -2,6 +2,9 @@ package array;
 
 public class ExtentionFor {
 	public static void main(String[] args) {
-		
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int value : scores) {
+			System.out.println(value);
+		}
 	}
 }
