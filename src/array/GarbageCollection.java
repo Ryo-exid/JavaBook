77ef916343938927c8eeda2f6a980252e195206f
Ryo-exid <sakura4oneok@gmail.com>
2021-09-12 22:21:49 +0900

    ガベージコレクション

--- src/array/GarbageCollection.java
+++ src/array/GarbageCollection.java
@@ -2,6 +2,10 @@ package array;
 
 public class GarbageCollection {
 	public static void main(String[] args) {
-		
+		boolean judge = true;
+		if (judge == true) {
+			int[] arrayG = {1, 2, 3}; // ifブロック内で配列を作成
+			System.out.println(arrayG[0]);
+		} // この時点で配列arrayはメモリから消滅（事実上メモリ内のゴミ（garbage）となる。）
 	}
 }
