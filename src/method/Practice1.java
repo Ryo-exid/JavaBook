82dab162a027065d08a9fa59dd34dae6ca44352b
Ryo-exid <sakura4oneok@gmail.com>
2021-09-14 20:50:15 +0900

    練習問題5-1

---
+++ src/method/Practice1.java
@@ -0,0 +1,18 @@
+package method;
+
+public class Practice1 {
+	public static void introduceOneself() {
+		String name = "ジャノメ";
+		int age = 8;
+		double height = 100;
+		char zodiac = '酉';
+		System.out.println("名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("身長は" + height + "cmです。");
+		System.out.println("十二支は" + zodiac + "です。");
+	}
+	
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+}
