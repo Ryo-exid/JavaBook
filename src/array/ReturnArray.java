b6628290c456aba5355dfe1af243ab4fdb9dca89
Ryo-exid <sakura4oneok@gmail.com>
2021-09-14 19:54:18 +0900

    戻り値に配列を用いる

---
+++ src/array/ReturnArray.java
@@ -0,0 +1,22 @@
+package array;
+
+public class ReturnArray {
+	
+	// int型配列を作成して戻すメソッド
+	public static int[] makeArray(int size) {
+		int[] newArray = new int[size];
+		for (int i = 0; i < newArray.length; i++) {
+			newArray[i] = i;
+		}
+		return newArray; // 配列を戻す
+	}
+	
+	public static void main(String[] args) {
+		int[] array = makeArray(3);
+		
+		// Arrayの全要素を出力
+		for (int i : array) {
+			System.out.println(i);
+		}
+	}
+}
