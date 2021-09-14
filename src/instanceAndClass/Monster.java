b7752a09a31f2d10721a322af1c9173f86590fff
Ryo-exid <sakura4oneok@gmail.com>
2021-09-15 00:27:21 +0900

    おばけキノコクラスの定義

---
+++ src/instanceAndClass/Monster.java
@@ -0,0 +1,10 @@
+package instanceAndClass;
+
+public class Monster {
+	int hp;
+	final int LEVEL = 10;
+	char suffix;
+	public void run() {
+		System.out.println("おばけキノコ" + this.suffix + "は逃げ出した！");
+	}
+}
