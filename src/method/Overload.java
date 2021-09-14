4c6a37a0d1caef9f0c96ed269445555c145cb64a
Ryo-exid <sakura4oneok@gmail.com>
2021-09-14 17:12:10 +0900

    仮引数の型や個数が異なれば同じ名前のメソッドを複数定義することが許される

--- src/method/Overload.java
+++ src/method/Overload.java
@@ -1,8 +1,8 @@
 package method;
 
-public class Overload1 {
+public class Overload {
 	
-	// 仮引数が異なれば同じ名前のメソッドを複数定義することが許されている。
+	// 仮引数がの型が異なれば同じ名前のメソッドを複数定義することが許されている。
 	
 	// 1つ目のaddメソッド
 	public static int add(int x, int y) {
@@ -19,9 +19,15 @@ public class Overload1 {
 		return x + y;
 	}
 	
+	// 4つ目のaddメソッド（引数の個数を変える場合）
+	public static int add(int x, int y, int z) {
+		return x + y + z;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(add(20, 30));
 		System.out.println(add(3.5, 2.7));
 		System.out.println(add("Hello", "World"));
+		System.out.println(add(100, 200, 300));
 	}
 }
