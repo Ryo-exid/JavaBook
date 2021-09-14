fca3d732dd153e777706d554d22f72bba5e9d89e
Ryo-exid <sakura4oneok@gmail.com>
2021-09-15 00:36:08 +0900

    仮想世界に勇者とおばけキノコ2匹を生み出す

--- src/instanceAndClass/Main.java
+++ src/instanceAndClass/Main.java
@@ -10,9 +10,19 @@ public class Main {
 		h.hp = 100;
 		System.out.println("勇者" + h.name + "を生み出しました！");
 		
-		// 3.勇者のメソッドを呼び出していく
+		Monster m1 = new Monster(); // おばけキノコ1匹目を生成し初期化
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		Monster m2 = new Monster(); // おばけキノコ2匹目を生成し初期化
+		m2.hp = 40;
+		m2.suffix = 'B';
+		
+		// 3.メソッドを呼び出していく
 		h.sit(5);
 		h.slip();
+		m1.run();
+		m2.run();
 		h.sit(25);
 		h.run();
 	}
