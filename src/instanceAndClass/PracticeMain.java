f3127a6c10c8b82466706d764e16cc45fd98d20f
Ryo-exid <sakura4oneok@gmail.com>
2021-09-15 01:25:23 +0900

    Clericクラスで定義したメソッドを実行

---
+++ src/instanceAndClass/PracticeMain.java
@@ -0,0 +1,10 @@
+package instanceAndClass;
+
+public class PracticeMain {
+	public static void main(String[] args) {
+		Cleric c = new Cleric();
+		c.name = "ぴよ子";
+		c.selfAid();
+		c.pray(2);
+	}
+}
