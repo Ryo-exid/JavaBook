00c581463bf2de82a5fdcbae52f1b6ae5a2055f0
Ryo-exid <sakura4oneok@gmail.com>
2021-09-16 00:27:39 +0900

    別のコンストラクタを呼び出す/this(引数);

--- src/variousClass/Hero.java
+++ src/variousClass/Hero.java
@@ -7,15 +7,17 @@ public class Hero {
 	// Sword型の変数
 	Sword sword; // あるクラスが別クラスをフィールドとして利用している関係を「has-a の関係」という。
 	
-	// コンストラクタ
+	// コンストラクタ①
 	public Hero(String name) {
 		this.hp = 100;
 		this.name = name;
 	}
-	// コンストラクタのオーバーロード
+	// コンストラクタ② ※オーバーロード
 	public Hero() {
-		this.hp = 100;
-		this.name = "ダミー";
+		// 同一クラス内の別のコンストラクタの呼び出し
+		// this(引数);
+		
+		this("ダミー"); // コンストラクタ①を呼び出しつつ、this.name = "ダミー"; と書くのと同じ意味
 	}
 	
 	public void attack() {
