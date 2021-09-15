3e9c051ba0bf46cd8f9fd0a985151b66f492748d
Ryo-exid <sakura4oneok@gmail.com>
2021-09-15 21:02:13 +0900

    攻撃時に「（名前）は（武器名）で攻撃した！」と表示されるように変更

--- src/variousClass/Hero.java
+++ src/variousClass/Hero.java
@@ -8,7 +8,7 @@ public class Hero {
 	Sword sword; // あるクラスが別クラスをフィールドとして利用している関係を「has-a の関係」という。
 	
 	public void attack() {
-		System.out.println(this.name + "は攻撃した！");
+		System.out.println(this.name + "は" + sword.name + "で攻撃した！");
 		System.out.println("敵に5ポイントのダメージを与えた！");
 	}
 }
