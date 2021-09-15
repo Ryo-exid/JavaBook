0b73d35a1ebefd5fdeb9a902846fdd8f4d1dfd0e
Ryo-exid <sakura4oneok@gmail.com>
2021-09-16 00:15:46 +0900

    コンストラクタのオーバーロード

--- src/variousClass/Hero.java
+++ src/variousClass/Hero.java
@@ -7,6 +7,17 @@ public class Hero {
 	// Sword型の変数
 	Sword sword; // あるクラスが別クラスをフィールドとして利用している関係を「has-a の関係」という。
 	
+	// コンストラクタ
+	public Hero(String name) {
+		this.hp = 100;
+		this.name = name;
+	}
+	// コンストラクタのオーバーロード
+	public Hero() {
+		this.hp = 100;
+		this.name = "ダミー";
+	}
+	
 	public void attack() {
 		System.out.println(this.name + "は" + sword.name + "で攻撃した！");
 		System.out.println("敵に5ポイントのダメージを与えた！");
