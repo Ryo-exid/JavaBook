a0e4bc88f810d9f4f3485a783fd0aa2735596af9
Ryo-exid <sakura4oneok@gmail.com>
2021-09-16 13:45:41 +0900

    runメソッドを再定義（オーバーライド）

--- src/inheritance/SuperHero.java
+++ src/inheritance/SuperHero.java
@@ -12,4 +12,9 @@ public class SuperHero extends Hero {
 		this.flying = false;
 		System.out.println("着陸した！");
 	}
+	
+	// オーバーライド（親クラスにあるメソッドを再定義）
+	public void run() {
+		System.out.println(this.name + "は撤退した");
+	}
 }
