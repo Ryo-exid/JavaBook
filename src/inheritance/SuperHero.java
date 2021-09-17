001f78bfd44fbe0ca73b6d8eb545d7b45bd8acaa
Ryo-exid <sakura4oneok@gmail.com>
2021-09-17 16:15:49 +0900

    親クラスのattackを2回呼び出す/super.メソッド名(引数)

--- src/inheritance/SuperHero.java
+++ src/inheritance/SuperHero.java
@@ -3,6 +3,14 @@ package inheritance;
 public class SuperHero extends Hero {
 	boolean flying;
 	
+	public void attack(Monster m) {
+		super.attack(m); // 親インスタンスのattackメソッドを呼び出し
+		// 飛んでいる状態なら2回攻撃
+		if (this.flying) {
+			super.attack(m);
+		}
+	}
+	
 	public void fly() {
 		this.flying = true;
 		System.out.println("飛び上がった！");
@@ -10,11 +18,11 @@ public class SuperHero extends Hero {
 	
 	public void land() {
 		this.flying = false;
-		System.out.println("着陸した！");
+		System.out.println(this.name + "は着陸した！");
 	}
 	
 	// オーバーライド（親クラスにあるメソッドを再定義）
 	public void run() {
-		System.out.println(this.name + "は撤退した");
+		System.out.println(this.name + "は撤退した。");
 	}
 }
