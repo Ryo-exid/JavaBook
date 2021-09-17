e3ce44d9765ef50d47bc4b8f6ed650a8914c52c7
Ryo-exid <sakura4oneok@gmail.com>
2021-09-17 18:08:49 +0900

    インスタンス生成/メソッド実行

---
+++ src/abstractAndInterface/Main.java
@@ -0,0 +1,9 @@
+package abstractAndInterface;
+
+public class Main {
+	public static void main(String[] args) {
+		Dancer d1 = new Dancer("踊り子");
+		Matango m1 = new Matango("クリボー");
+		d1.attack(m1);
+	}
+}
