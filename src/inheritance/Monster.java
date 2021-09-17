37184f2e7ad75e9384075a037e9b5a7429c2c0a4
Ryo-exid <sakura4oneok@gmail.com>
2021-09-17 17:23:29 +0900

    練習問題10-3/Monsterクラスを定義

--- src/inheritance/Monster.java
+++ src/inheritance/Monster.java
@@ -1,10 +1,20 @@
 package inheritance;
 
 public class Monster {
-	String name;
-	int hp = 20;
+	int hp = 50;
+	char suffix;
 	
-	public Monster(String name) {
-		this.name = name;
+	public Monster(char suffix) {
+		this.suffix = suffix;
+	}
+	
+	public void attack(Hero h) {
+		System.out.println("キノコ" + this.suffix + "の攻撃");
+		System.out.println("10のダメージ！");
+		h.hp -= 10;
+	}
+	
+	public void run() {
+		System.out.println("キノコ" + this.suffix + "は逃げ出した！");
 	}
 }
