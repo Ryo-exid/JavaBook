385aa57c6309fbd9dec057034179f8307efd03f4
Ryo-exid <sakura4oneok@gmail.com>
2021-09-17 16:37:00 +0900

    子クラスのインスタンスを生成

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -2,9 +2,7 @@ package inheritance;
 
 public class Main {
 	public static void main(String[] args) {
-		SuperHero sh = new SuperHero();
-		sh.name = "スーパーヒーロー";
-		
+		SuperHero sh = new SuperHero("スーパーヒーロー");
 		Monster m1 = new Monster("クリボー");
 		
 		sh.fly();
