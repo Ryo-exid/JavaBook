07017ed03bdaeedc45c95489cc75f9ac510d8ab3
Ryo-exid <sakura4oneok@gmail.com>
2021-09-17 16:21:51 +0900

    SuperHeroクラスで定義したメソッドを実行

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -4,12 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		SuperHero sh = new SuperHero();
 		sh.name = "スーパーヒーロー";
-		sh.run();
 		
 		Monster m1 = new Monster("クリボー");
 		
 		sh.fly();
 		sh.attack(m1); // flyingがtrueの間は2回攻撃
 		sh.land();
+		sh.run();
 	}
 }
