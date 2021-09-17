9007ad5685ad6de09c6f799d9ba8e7a274af41c7
Ryo-exid <sakura4oneok@gmail.com>
2021-09-17 17:28:25 +0900

    練習問題10-3/PoisonMonsterクラスのインスタンス生成・メソッド実行

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -3,11 +3,13 @@ package inheritance;
 public class Main {
 	public static void main(String[] args) {
 		SuperHero sh = new SuperHero("スーパーヒーロー");
-		Monster m1 = new Monster("クリボー");
+		PoisonMonster pm = new PoisonMonster('A');
 		
+		pm.attack(sh);
 		sh.fly();
-		sh.attack(m1); // flyingがtrueの間は2回攻撃
+		sh.attack(pm); // flyingがtrueの間は2回攻撃
 		sh.land();
+		pm.run();
 		sh.run();
 	}
 }
