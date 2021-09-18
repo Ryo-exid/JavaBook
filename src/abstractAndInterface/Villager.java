bc642214c117d2cb827def8313a74c9367c687bf
Ryo-exid <sakura4oneok@gmail.com>
2021-09-18 20:41:18 +0900

    extends/implements

--- src/abstractAndInterface/Villager.java
+++ src/abstractAndInterface/Villager.java
@@ -1,15 +1,19 @@
 package abstractAndInterface;
 
 public class Villager extends Character implements Human {
+	// Characterからnameやhpなどのフィールドを継承している。
 	
+	// コンストラクタ
 	public Villager(String name) {
 		super(name);
 	}
 	
+	// Characterから継承した抽象メソッドattack()を実装
 	public void attack(Matango m) {
 		System.out.println(this.name + "は戦わず遊んでいる");
 	}
 	
+	// さらにHumanから継承した4つの抽象メソッドを実装
 	public void talk() {
 		System.out.println(this.name + "は話す");
 	}
@@ -23,3 +27,10 @@ public class Villager extends Character implements Human {
 		System.out.println(this.name + "は逃げる");
 	}
 }
+
+// implements（実装）とextends（拡張）の使い分け
+
+// 継承元　→　継承先
+// クラス　→　クラス  ...extends
+// インターフェース　→　クラス  ...implements
+// インターフェース　→　インターフェース  ...extends
