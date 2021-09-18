f0bdcb09de135e41ad19f5b78ad5f93975c85035
Ryo-exid <sakura4oneok@gmail.com>
2021-09-18 22:36:41 +0900

    コンピュータという資産を管理するプログラム（TangibleAssetクラスを継承）

--- src/abstractAndInterfacePractice/Computer.java
+++ src/abstractAndInterfacePractice/Computer.java
@@ -1,24 +1,16 @@
 package abstractAndInterfacePractice;
 
 //会社の資産として保有するもの（コンピュータ）を管理するプログラム
-public class Computer {
+public class Computer extends TangibleAsset {
 	// フィールド
-	String name;
-	int price;
-	String color;
 	String makerName;
 	
 	// コンストラクタ
 	public Computer(String name, int price, String color, String makerName) {
-		this.name = name;
-		this.price = price;
-		this.color = color;
+		super(name, price, color);
 		this.makerName = makerName;
 	}
 	
 	// メソッド
-	public String getname() { return this.name; }
-	public int getPrice() { return this.price; }
-	public String getColor() { return this.color; }
 	public String makerName() { return this.makerName; }
 }
