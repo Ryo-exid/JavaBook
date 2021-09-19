e953cc371a57915d8dcac0b5c18814316bda2a58
Ryo-exid <sakura4oneok@gmail.com>
2021-09-19 20:58:13 +0900

    ダウンキャスト

--- src/polymorphism/Main.java
+++ src/polymorphism/Main.java
@@ -8,9 +8,16 @@ public class Main {
 		w.fireball(ma);
 		System.out.println(ma.name + "の残りHP：" + ma.hp);
 		
-		Character c = w;
-		c.attack(ma);
-//		c.fireball(m);  // エラーになる（Characterという型の箱に入れ直したことで、Characterとして持つメソッドしか呼び出すことができない）
+		Character c1 = w;
+		c1.attack(ma);
+//		c1.fireball(m);  // エラーになる（Characterという型の箱に入れ直したことで、Characterとして持つメソッドしか呼び出すことができない）
+		
+		// どうしてもfireballを使いたい場合（ダウンキャスト） ※失敗の危険が伴う
+		Character c2 = new Wizard("セニカ");
+		Wizard ww = (Wizard)c2; // 曖昧な型（Caracter）だったc2を、強制的に具体的な型（Wizard）として捉える
+		ww.fireball(ma);
+		
+		System.out.println("=============================================");
 		
 		Slime s = new Slime();
 		Monster mo = new Slime();
@@ -21,3 +28,10 @@ public class Main {
 	    //  ※「メソッドの動き」は箱の型（Monster型）ではなく、中身の型（Slime型）で決まる
 	}
 }
+
+/* キャストしても大丈夫かどうかをチェック（instanceof演算子）
+	if (c instanceof SuperHero) {  // もしcの中身がSuperHeroと見なして大丈夫なら...
+		SuperHero h = (SuperHero)c;  // SuperHeroと見なせ！
+		h.fly();
+	}
+*/
