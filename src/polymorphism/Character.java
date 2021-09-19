0c175cd0e0793793b1a48e333069c3e82e2e0a98
Ryo-exid <sakura4oneok@gmail.com>
2021-09-19 20:09:34 +0900

    抽象クラス/抽象メソッドを定義

---
+++ src/polymorphism/Character.java
@@ -0,0 +1,10 @@
+package polymorphism;
+
+public abstract class Character {
+	String name;
+	int hp;
+	public void run() {
+		System.out.println(this.name + "は逃げた！");
+	}
+	public abstract void attack(Matango m);
+}
