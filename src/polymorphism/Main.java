503a1e30412c2925e8c6e3b391416da194e522d0
Ryo-exid <sakura4oneok@gmail.com>
2021-09-19 20:29:34 +0900

    MonsterとSlimeの逃げ方を調べる

--- src/polymorphism/Main.java
+++ src/polymorphism/Main.java
@@ -3,13 +3,21 @@ package polymorphism;
 public class Main {
 	public static void main(String[] args) {
 		Wizard w = new Wizard("ゼシカ");
-		Matango m = new Matango("おばけキノコ");
-		w.attack(m);
-		w.fireball(m);
-		System.out.println(m.name + "の残りHP：" + m.hp);
+		Matango ma = new Matango("おばけキノコ");
+		w.attack(ma);
+		w.fireball(ma);
+		System.out.println(ma.name + "の残りHP：" + ma.hp);
 		
 		Character c = w;
-		c.attack(m);
+		c.attack(ma);
 //		c.fireball(m);  // エラーになる（Characterという型の箱に入れ直したことで、Characterとして持つメソッドしか呼び出すことができない）
+		
+		Slime s = new Slime();
+		Monster mo = new Slime();
+		
+		// 以下、どちらも "スライムはサササッと逃げ出した。" という実行結果になる
+		s.run();
+		mo.run();
+	    //  ※「メソッドの動き」は箱の型（Monster型）ではなく、中身の型（Slime型）で決まる
 	}
 }
