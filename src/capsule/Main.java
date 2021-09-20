da0e61457c6f49334842b7ecbbd83c195a6a4796
Ryo-exid <sakura4oneok@gmail.com>
2021-09-20 21:25:12 +0900

    練習問題13-1〜4/インスタンス生成・メソッドを実行

--- src/capsule/Main.java
+++ src/capsule/Main.java
@@ -6,7 +6,17 @@ public class Main {
 		System.out.println(svc.getName()); // ゼシカ
 		svc.setName("魔法使いゼシカ");
 		System.out.println(svc.getName()); // 魔法使いゼシカ
+//		svc.setName(""); // エラーメッセージ "名前が短すぎる。処理を中断。"
 		
-		svc.setName(""); // エラーメッセージ "名前が短すぎる。処理を中断。"
+		System.out.println("======================================");
+		
+		Hero h = new Hero("勇者");
+		Wizard w = new Wizard("ゼシカ");
+		Wand specialWand = new Wand("特別な魔法の杖");
+		specialWand.setPower(50);
+		w.setWand(specialWand);
+		System.out.println("勇者のHP：" + h.getHp()); // 勇者のHP：100
+		w.heal(h); // ゼシカは勇者のHPを500回復した！
+		System.out.println("勇者のHP：" + h.getHp()); // 勇者のHP：600
 	}
 }
