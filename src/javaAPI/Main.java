883996c442e4486400ecfc706f8618a0abae445c
Ryo-exid <sakura4oneok@gmail.com>
2021-09-21 20:00:53 +0900

    オーバーライドしたequalsメソッドを実行

--- src/javaAPI/Main.java
+++ src/javaAPI/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		// System.out.println()という命令は、渡されたObjectのtoStringメソッドを呼び出して得られる文字列表現を画面に表示するため、以下も同様の結果となる。
 		System.out.println(h1); // 名前：リョウ / HP：100
 		
-		Hero h2 = new Hero("ゼシカ");
+		Hero h2 = new Hero("リョウ");
 		if (h1.equals(h2) == true) {
 			System.out.println("同じ内容です");
 		} else {
@@ -15,5 +15,3 @@ public class Main {
 		}
 	}
 }
-
-// equals() ...等価（同じ内容）かどうかを判定する  ※「==」は等値（同一の存在）であることを判定するものなので違いに注意
