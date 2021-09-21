915caa664294037a75c2e0e28a412b8bb96e44de
Ryo-exid <sakura4oneok@gmail.com>
2021-09-21 20:00:25 +0900

    名前が同じなら同じ勇者とするequalsメソッドを定義（オーバーライド）

--- src/javaAPI/Hero.java
+++ src/javaAPI/Hero.java
@@ -14,6 +14,21 @@ public class Hero {
 	public String toString() {
 		return "名前：" + this.name + " / HP：" + this.hp;
 	}
+	
+	// 名前が同じなら同じ勇者とみなすequals()  ※オーバーライド
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true; // 等値（同一の存在）なら間違いなく等価
+		}
+		if (o instanceof Hero) {
+			Hero h = (Hero)o;
+			if (this.name.equals(h.name)) { // 等しければ等価（同じ内容であること）
+				return true;
+			}
+		}
+		return false;
+	}
 }
 
 // ・toString()  ...自分自身の内容の文字列表現を返す
+// ・equals() ...等価（同じ内容）かどうかを判定する  ※「==」は等値（同一の存在）であることを判定するものなので違いに注意
