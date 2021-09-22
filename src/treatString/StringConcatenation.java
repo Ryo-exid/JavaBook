3e57dff700faa9892ae93dfb3dea9bc977b3a950
Ryo-exid <sakura4oneok@gmail.com>
2021-09-22 16:08:02 +0900

    StringBuilderを利用し、文字列を１万回連結する

--- src/treatString/StringConcatenation.java
+++ src/treatString/StringConcatenation.java
@@ -4,8 +4,8 @@ package treatString;
 public class StringConcatenation {
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder();
-		// 文字列を500回連結する ※万単位以上でもOK
-		for (int i = 0; i < 500; i++) {
+		// 文字列を10000回連結する
+		for (int i = 0; i < 10000; i++) {
 			sb.append("Java"); // appendメソッドで、バッファー（メモリ領域）に文字列を追加
 		}
 		String s = sb.toString(); // 最後に一度だけtoStringメソッドを呼び、連結させた文字列を取り出す
