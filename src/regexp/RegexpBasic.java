01d5ecaf3a883056ff2b45384b20b364918387ae
Ryo-exid <sakura4oneok@gmail.com>
2021-09-22 16:41:04 +0900

    matchesメソッド/ピリオド：任意の1文字であれば良い

--- src/regexp/RegexpBasic.java
+++ src/regexp/RegexpBasic.java
@@ -5,5 +5,8 @@ public class RegexpBasic {
 		String s = "Java";
 		System.out.println(s.matches("Java")); // true
 		System.out.println(s.matches("java")); // false
+		
+		// ピリオド：任意の1文字であれば良い
+		System.out.println("Java".matches("J.va")); // true
 	}
 }
