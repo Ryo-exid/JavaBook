94c9f3d43f89b5e175f7598f8f521dd92e15b85e
Ryo-exid <sakura4oneok@gmail.com>
2021-09-22 23:54:41 +0900

    6つのint値とDate型の相互変換

---
+++ src/dateAndTime/CalendarAndDate.java
@@ -0,0 +1,22 @@
+package dateAndTime;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarAndDate {
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance();
+		
+		// 6つのint値からDateインスタンスを生成
+		c.set(2019,8,22,1,23,45);
+		c.set(Calendar.MONTH, 9); // 月を9月に変更
+		Date d = c.getTime();
+		System.out.println(d);
+		
+		// Dateインスタンスからint値を生成
+		Date now = new Date();
+		c.setTime(now);
+		int y = c.get(Calendar.YEAR);
+		System.out.println("今年は" + y + "年です");
+	}
+}
