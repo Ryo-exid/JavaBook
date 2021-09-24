7a6dab596db4b7d166a1b4ee13eb176c87db9a11
Ryo-exid <sakura4oneok@gmail.com>
2021-09-25 00:14:29 +0900

    練習問題16-2/16-3

--- src/collectionPractice/Main.java
+++ src/collectionPractice/Main.java
@@ -1,17 +1,31 @@
 package collectionPractice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
+		// 練習問題16-2
 		Hero h1 = new Hero("斉藤");
 		Hero h2 = new Hero("鈴木");
-		List<Hero> heros = new ArrayList<Hero>();
-		heros.add(h1);
-		heros.add(h2);
-		for (Hero h : heros) {
+		List<Hero> heroes = new ArrayList<Hero>();
+		heroes.add(h1);
+		heroes.add(h2);
+		for (Hero h : heroes) {
 			System.out.println("勇者：" + h.getName());
+		}
+		
+		System.out.println("====================");
+		
+		// 練習問題16-3
+		Map<Hero, Integer> neoHeroes = new HashMap<Hero, Integer>();
+		neoHeroes.put(h1, 3);
+		neoHeroes.put(h2, 7);
+		for (Hero nh : neoHeroes.keySet()) {
+			int value = neoHeroes.get(nh);
+			System.out.println(nh.getName() + "が倒した的＝" + value);
 		}
 	}
 }
