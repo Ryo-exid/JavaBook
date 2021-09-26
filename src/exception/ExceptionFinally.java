737db9a6c73e51f11fafe83508d953793d5f682b
Ryo-exid <sakura4oneok@gmail.com>
2021-09-26 16:47:20 +0900

    後付け処理（finally）/close()にまつわる解決

---
+++ src/exception/ExceptionFinally.java
@@ -0,0 +1,27 @@
+package exception;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ExceptionFinally {
+	public static void main(String[] args) {
+		FileWriter fw = null; // 例外が生じた場合に備えてnullを代入し、初期化しておく。
+		try {
+			fw = new FileWriter("data2.text");
+			fw.write("Good morning!");
+		} catch (Exception e) {
+			System.out.println("何らかの例外が発生しました");
+		} finally { // 後付け処理もtry-catchする（18行目がIOExceptionを発生させる可能性があるメソッドであるため）
+			// fwがnullではない時だけclose()を試みる ※10行目で例外が発生すると、fwにはnullが入っているためNullPointerExceptionが発生してしまう＝nullということはファイルのオープンに失敗しているためcloseする必要がない
+			if (fw != null) {
+				try {
+					fw.close(); // IOExceptionを発生させる可能性があるメソッド
+				} catch(IOException e) {
+					; // 空文（ここでは何もしない） ※「ファイルが閉じられない」時にできることはない
+				}
+			}
+		}
+	}
+}
+
+// もっと上記がシンプルになるコード → TryWithResources.java
