7e053aa3f62d9dfcfba5c70ab184a7f69d724821
Ryo-exid <sakura4oneok@gmail.com>
2021-09-26 17:39:26 +0900

    オリジナル例外を利用する

--- src/exception/Main.java
+++ src/exception/Main.java
@@ -2,7 +2,19 @@ package exception;
 
 public class Main {
 	public static void main(String[] args) {
+		// Person.java
 		Person p = new Person();
 		p.setAge(-128); // 例外発生
+		
+		
+		/*
+		// UnsupportedMusicFileException.java
+		try {
+			// 試験的に例外を発生させる
+			throw new UnsupportedMusicFileException("未対応のファイルです");
+		} catch (Exception e) {
+			e.printStackTrace(); // スタックトレースの内容を画面に出力するメソッド
+		}
+		*/
 	}
 }
