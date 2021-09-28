2fd9e1b3803911df1f42c8f3dd540f381e967edf
Ryo-exid <sakura4oneok@gmail.com>
2021-09-28 22:07:51 +0900

    Enum（列挙型）

---
+++ src/wcp/enumeration/Main.java
@@ -0,0 +1,9 @@
+package wcp.enumeration;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(Dessert.APPLE.getPrice());
+		System.out.println(Dessert.ICE_CREAM.getPrice());
+		System.out.println(Dessert.CAKE.getPrice());
+	}
+}
