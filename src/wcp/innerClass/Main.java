791c7183c74927bfbc9e53de62e06bf877ecce30
Ryo-exid <sakura4oneok@gmail.com>
2021-09-28 17:22:52 +0900

    内部クラスのメソッドを実行

---
+++ src/wcp/innerClass/Main.java
@@ -0,0 +1,14 @@
+package wcp.innerClass;
+
+public class Main {
+	public static void main(String[] args) {
+		// 内部クラス / ”蹴る”という文字列を表示させる
+		Human human = new Human();
+		// ※内部クラスは直接参照できない為、型として用いる際はHuman.HumanLegと外部クラスと共に指定するか、import文を書く
+		Human.HumanLeg leg = human.new HumanLeg(); // human.new HumanLeg()と、外部クラスのHumanを使ってインスタンスを作る
+		leg.kick();
+		
+		// 上記は1行で表すことも可能
+		new Human().new HumanLeg().kick();
+	}
+}
