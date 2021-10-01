9dd62cc217079787c44368931c6774da09b00300
Ryo-exid <sakura4oneok@gmail.com>
2021-10-01 22:02:21 +0900

    キャスト/int,double

---
+++ src/selfLearning/Cast1.java
@@ -0,0 +1,10 @@
+package selfLearning;
+
+public class Cast1 {
+	public static void main(String[] args) {
+		double d = 0.12345678 * 10000;
+		int i = (int)d;
+		
+		System.out.println(i); // 1234
+	}
+}
